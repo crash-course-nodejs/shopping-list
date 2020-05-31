417029eb1b36e6ea8bd372874e3dfda1e79bf5c2
benny.back <benny.back@kakaocorp.com>
2020-05-31 23:55:27 +0900

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose'); // ORM with mongoDB, mongoDB Driver can use it.
 const bodyParser = require('body-parser'); // express 의 res, req 의 body를 처리하는 미들웨어
+const path = require('path');
 
 // api/items/*
 const items = require('./routes/api/items');
@@ -21,6 +22,16 @@ mongoose
 
 // Use Routes
 app.use('/api/items', items);
+
+// 프로덕션 모드일 때
+if (process.env.NODE_ENV === 'production') {
+  // static 폴더 설정
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000; // Heroku 의 포트
 
