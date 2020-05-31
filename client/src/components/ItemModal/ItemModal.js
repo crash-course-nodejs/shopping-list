9c6184f8476028b3bda4ff02a6a005120b11ba7b
benny.back <benny.back@kakaocorp.com>
2020-05-31 23:36:14 +0900

    items api 연결완료

--- client/src/components/ItemModal/ItemModal.js
+++ client/src/components/ItemModal/ItemModal.js
@@ -11,21 +11,19 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../../actions/itemActions';
-import { v4 as uuidv4 } from 'uuid';
 
 const ItemModal = ({addItem}) => {
   const [modal, setModal] = useState(false);
   const [name, setName] = useState('');
 
   const toggle = () => setModal(!modal);
-  const onChange = (e) => {
+  const onChange = e => {
     setName(e.target.value);
   };
-  const onSubmit = (e) => {
+  const onSubmit = e => {
     e.preventDefault();
 
     const newItem = {
-      id: uuidv4(),
       name
     };
 
